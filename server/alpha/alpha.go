94c119b0fc75545596202e73973c389610f55da1
Kaixuan Luo <lkx13662254197@163.com>
2025-01-07 18:00:19 +0800

    feat: 去除加密货币，数据量过少

--- server/alpha/alpha.go
+++ server/alpha/alpha.go
@@ -68,28 +68,6 @@ func AlphaDaily() {
 	if err != nil {
 		zap.S().Errorf("Insert Error: %v", err)
 	}
-
-	// 查询type 3 加密货币参数名称
-	alphaCryptoInfos, err := alphaRepo.GetSymbolByType(public.CryptoType)
-	if err != nil {
-		zap.S().Errorf("GetAlphaCrypto Error: %v", err)
-	}
-	cryptoList := make([]*models.Crypto, 0, len(alphaCryptoInfos))
-	for _, alphaInfo := range alphaCryptoInfos {
-		alpha, err := GetAlphaCrypto(alphaInfo.Symbol)
-		if err != nil {
-			zap.S().Errorf("GetAlphaCrypto Error: %v", err)
-			continue
-		}
-		cryptoList = append(cryptoList, alpha)
-	}
-	cryptoRepo := models.NewCryptoRepo(ctx)
-	err = cryptoRepo.Insert(cryptoList)
-	if err != nil {
-		zap.S().Errorf("Insert Error: %v", err)
-	}
-
-	return
 }
 
 func GetAlphaStock(symbol string) (*models.Stock, error) {
@@ -166,45 +144,6 @@ func GetAlphaCurrency(fromSymbol, toSymbol string) (*models.Currency, error) {
 		Close: cast.ToFloat64(yesterdayData.Close),
 	}
 	return currency, nil
-}
-
-func GetAlphaCrypto(symbol string) (*models.Crypto, error) {
-	client := resty.New()
-	get, err := client.R().SetQueryParam("function", dailyCurrencyFunc).
-		SetQueryParam("symbol", symbol).
-		SetQueryParam("market", market).
-		SetQueryParam("apikey", config.Configs.Alpha.ApiKey).
-		Get(url)
-	if err != nil {
-		zap.S().Errorf("GetAlphaCurrency Error: %v", err)
-		return nil, err
-	}
-
-	resp := &CryptoResp{}
-	err = json.Unmarshal(get.Body(), resp)
-	if err != nil {
-		zap.S().Errorf("GetAlphaCurrency Error: %v", err)
-		return nil, err
-	}
-
-	yesterdayData, ok := resp.TimeSeriesDaily[getYesterdayStr()]
-	zap.S().Debugf("GetAlphaCurrency yesterdayData: %+v", yesterdayData)
-	if !ok {
-		zap.S().Debug("No data for yesterday")
-		return nil, nil
-	}
-
-	crypto := &models.Crypto{
-		Currency: symbol,
-		Date:     time.Now().AddDate(0, 0, -1),
-		Open:     cast.ToFloat64(yesterdayData.Open),
-		High:     cast.ToFloat64(yesterdayData.High),
-		Low:      cast.ToFloat64(yesterdayData.Low),
-		Close:    cast.ToFloat64(yesterdayData.Close),
-		Volume:   cast.ToFloat64(yesterdayData.Volume),
-	}
-
-	return crypto, nil
 }
 
 func InitInsertStockData(symbol string) {
@@ -299,55 +238,6 @@ func InitInsertCurrencyData(fromSymbol, toSymbol string) {
 	})
 	currencyRepo := models.NewCurrencyRepo(context.Background())
 	err = currencyRepo.Insert(currencyList)
-	if err != nil {
-		zap.S().Errorf("Insert Error: %v", err)
-		return
-	}
-	return
-}
-
-func InitInsertCryptoData(symbol string) {
-	client := resty.New()
-	get, err := client.R().SetQueryParam("function", dailyCryptoFunc).
-		SetQueryParam("symbol", symbol).
-		SetQueryParam("market", market).
-		SetQueryParam("apikey", config.Configs.Alpha.ApiKey).
-		Get(url)
-	if err != nil {
-		zap.S().Errorf("GetAlphaStock Error: %v", err)
-		return
-	}
-	resp := &CryptoResp{}
-	err = json.Unmarshal(get.Body(), resp)
-	if err != nil {
-		zap.S().Errorf("GetAlphaStock Error: %v", err)
-		return
-	}
-
-	cryptoList := make([]*models.Crypto, 0, len(resp.TimeSeriesDaily))
-	for dateStr, data := range resp.TimeSeriesDaily {
-		date, _ := time.Parse(time.DateOnly, dateStr)
-		stock := &models.Crypto{
-			Currency: symbol,
-			Date:     date,
-			Open:     cast.ToFloat64(data.Open),
-			High:     cast.ToFloat64(data.High),
-			Low:      cast.ToFloat64(data.Low),
-			Close:    cast.ToFloat64(data.Close),
-			Volume:   cast.ToFloat64(data.Volume),
-		}
-		cryptoList = append(cryptoList, stock)
-	}
-	slices.SortFunc(cryptoList, func(a, b *models.Crypto) int {
-		if a.Date.Before(b.Date) {
-			return -1
-		} else if a.Date.After(b.Date) {
-			return 1 // a 在 b 之后
-		}
-		return 0
-	})
-	cryptoRepo := models.NewCryptoRepo(context.Background())
-	err = cryptoRepo.Insert(cryptoList)
 	if err != nil {
 		zap.S().Errorf("Insert Error: %v", err)
 		return
